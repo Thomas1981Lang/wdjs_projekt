8fab575b0671cc0366f397198d73af3a507ced27
Thomas Lang <34832500+Thomas1981Lang@users.noreply.github.com>
2018-05-11 06:44:08 +0200

    for coaching

--- resources/js/singles.js
+++ resources/js/singles.js
@@ -37,7 +37,7 @@ var map;
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.7413549, lng: -73.9980244},
-        zoom: 13
+        center: {lat: 48.207715, lng: 16.375253},
+        zoom: 18
     });
 }
